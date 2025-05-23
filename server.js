d887a39632711b0d1858afa927c84c83237e7773
sudeepnn <sudeep2002naik@gmail.com>
2025-05-23 20:17:32 +0530

    SErver aws

--- server.js
+++ server.js
@@ -622,4 +622,5 @@ app.get('/api/blogvisits/export', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://0.0.0.0:${PORT}`));
+
